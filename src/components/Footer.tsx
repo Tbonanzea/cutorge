523a0584259b0e5ec13d2f801ac22275af2bb33e
Tomas Bonanzea <tomasbonanzea98@gmail.com>
2026-01-31 15:57:54 -0300

    fix: update copyright year in Footer component to 2026

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className='w-full bg-gray-800 text-white p-4 mt-auto'>
 			<div className='max-w-7xl mx-auto'>
-				<p>© 2025 - CutForge</p>
+				<p>© 2026 - CutForge</p>
 				<small>Todos los derechos reservados</small>
 			</div>
 		</footer>
